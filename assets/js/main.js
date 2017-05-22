712aa012d8d7f9e6673b8143f8c61dde986df7de
“Anais <ananavelez@gmail.com>
2017-05-21 20:54:43 -0400

    tea-lovers-II-casi casi terminado

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+ function validacion(){
+ 	var inputs=document.getElementsByClassName("validacion").value;
+ 	if (inputs===undefined||inputs==="") {
+ 		alert("hay error")
+ 	}else{
+     alert("gracias por Suscribirte")
+ 	}
+ }
+ validacion();
