e8cd2c96f225a9ea192b24c991843ae33005557f
“Anais <ananavelez@gmail.com>
2017-07-31 08:53:51 -0400

    bases

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
- function validacion(){
+/* function validacion(){
  	var inputs=document.getElementsByClassName("validacion").value;
  	if (inputs===undefined||inputs==="") {
  		alert("hay error")
@@ -6,4 +6,14 @@
      alert("gracias por Suscribirte")
  	}
  }
- validacion();
+ validacion();*/
+
+  $('.button-collapse').sideNav({
+      menuWidth: 1s00%, // Default is 300
+      edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true, // Choose whether you can drag to open on touch screens,
+      onOpen: function(el) { /* Do Stuff* / }, // A function to be called when sideNav is opened*/},
+      onClose: function(el) { /* Do Stuff* / }, // A function to be called when sideNav is closed*/};
+  })
+        
